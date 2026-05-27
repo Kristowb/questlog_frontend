b60f7af8f4d23d0ad4084ef2655ece8487a425a1
Kristianto Wibawa <kristiantow007@gmail.com>
2026-05-27 09:35:49 +0700

    chore(frontend): configure dynamic googleClientId based on kDebugMode using debug and release client secrets

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/foundation.dart';
+
 class AppConfig {
   static const String backendUrl = 'https://questlogbackend-production.up.railway.app/api/v1';
-  static const String googleClientId = '531228559747-j7173dv83osib4uhn68dvmmgefbi0d37.apps.googleusercontent.com';
+  
+  static const String googleClientId = kDebugMode
+      ? '531228559747-1ctb1pg7511ol94unltiiv4mb528uhqk.apps.googleusercontent.com' // Debug client ID
+      : '531228559747-04vh3kol55mm7mp4fvsavv7stio02ed1.apps.googleusercontent.com'; // Release client ID
 }
